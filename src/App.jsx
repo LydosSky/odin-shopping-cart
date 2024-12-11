913ab5e17b12bd70881dac0107c64fdd832b36ad
Lydos <lydossky@gmail.com>
2024-12-11 21:40:17 +0300

    chore: tailwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-function App() {
-    return <h1>Hello World</h1>;
+export default function App() {
+    return <h1 className="text-5xl  font-bold underline">Hello World</h1>;
 }
-
-export default App;
